f05d1c0ced9861fccfe16efc1a07c3f73e02bb8f
Reversible Card <78249745+Lighthigh57@users.noreply.github.com>
2021-05-05 09:54:14 +0900

    Fix Bug

--- Assets/Script/BoardManager.cs
+++ Assets/Script/BoardManager.cs
@@ -8,30 +8,6 @@ public class BoardManager : MonoBehaviour
     private readonly bool[,] boardcheck = new bool[8, 8];
     private readonly GameObject[,] disclist = new GameObject[8, 8];//y,x
     // Start is called before the first frame update
-    void Start()
-    {
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                boardinfo[i, j] = ((i * 8) + j) % 2;
-                Debug.Log(i * 8 + j);
-                if (boardinfo[i, j] == 0)
-                {
-                    boardcheck[i, j] = true;
-                }
-            }
-        }
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                disclist[i, j] = Instantiate(disc, new Vector3(-3.5f + j, 0.1f, 3.5f - i), Quaternion.identity);
-            }
-        }
-        Checkboard();
-    }
-
     private void Checkboard()
     {
 
@@ -49,13 +25,46 @@ public class BoardManager : MonoBehaviour
                     else
                     {
                         target.SetActive(true);
-                        target.GetComponent<Transform>().Rotate(180, 0, 0);
+                        if (boardinfo[i, j] == 0)
+                        {
+                            target.GetComponent<Animator>().SetBool("Color", false);
+                            boardinfo[i, j] = 1;
+                        }
+                        else
+                        {
+                            target.GetComponent<Animator>().SetBool("Color", true);
+                            boardinfo[i, j] = 0;
+                        }
                     }
                     boardcheck[i, j] = false;
                 }
             }
         }
     }
+    void Start()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                boardinfo[i, j] = ((i * 8) + j) % 2;
+                if (boardinfo[i, j] == 1)
+                {
+                    boardcheck[i, j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                disclist[i, j] = Instantiate(disc, new Vector3(-3.5f + j, 0.1f, 3.5f - i), Quaternion.identity);
+            }
+        }
+        Checkboard();
+    }
+
+    
 
     // Update is called once per frame
     void Update()
