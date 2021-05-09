ce4acdb51264e1d43e3b2abcc6f040ccb56962ea
Reversible Card <78249745+Lighthigh57@users.noreply.github.com>
2021-05-09 09:48:50 +0900

    Bug Fixed

--- Assets/Script/BoardManager.cs
+++ Assets/Script/BoardManager.cs
@@ -8,39 +8,6 @@ public class BoardManager : MonoBehaviour
     private readonly bool[,] boardcheck = new bool[8, 8];
     private readonly GameObject[,] disclist = new GameObject[8, 8];//y,x
     // Start is called before the first frame update
-    private void Checkboard()
-    {
-
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                if (boardcheck[i, j])
-                {
-                    GameObject target = disclist[i, j];
-                    if (boardinfo[i, j] < 0)
-                    {
-                        target.SetActive(false);
-                    }
-                    else
-                    {
-                        target.SetActive(true);
-                        if (boardinfo[i, j] == 0)
-                        {
-                            target.GetComponent<Animator>().SetBool("Color", false);
-                            boardinfo[i, j] = 1;
-                        }
-                        else
-                        {
-                            target.GetComponent<Animator>().SetBool("Color", true);
-                            boardinfo[i, j] = 0;
-                        }
-                    }
-                    boardcheck[i, j] = false;
-                }
-            }
-        }
-    }
     void Start()
     {
         for (int i = 0; i < 8; i++)
@@ -58,17 +25,44 @@ public class BoardManager : MonoBehaviour
         {
             for (int j = 0; j < 8; j++)
             {
-                disclist[i, j] = Instantiate(disc, new Vector3(-3.5f + j, 0.1f, 3.5f - i), Quaternion.identity);
+                disclist[i, j] = Instantiate(disc, new Vector3(-3.5f + j, 0, 3.5f - i), Quaternion.identity);
             }
         }
-        Checkboard();
     }
 
-    
+
 
     // Update is called once per frame
     void Update()
     {
-        //Checkboard();
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                int state = boardinfo[i, j];
+                GameObject disc = disclist[i, j];
+                if (state == 0)
+                {
+                    if (disc.activeSelf) disc.SetActive(false);
+                }
+                else
+                {
+                    if (!disc.activeSelf) disc.SetActive(true);
+                }
+                Debug.Log("state");
+                disc.GetComponent<Disc>().reload(state);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    boardinfo[i, j] = Random.Range(-1, 1);
+                }
+            }
+        }
     }
+
 }
---
+++ Assets/Script/Disc.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disc : MonoBehaviour
+{
+    int state = 1;
+    Rigidbody rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    public void reload(int remoteState)
+    {
+        if (state == remoteState)
+        {
+            return;
+        }
+
+        rig.AddForce(0, 15.0f, 0);
+        state = remoteState;
+    }
+
+    private void Update()
+    {
+        if (Mathf.Abs(transform.eulerAngles.x - (90 + 90 * state)) < 5)
+        {
+            rig.AddTorque(5.0f, 0, 0);
+        }
+
+    }
+}
