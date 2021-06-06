25ab4ac329734cd1aede6c455980c8aca6ed0cbf
Reversible Card <78249745+Lighthigh57@users.noreply.github.com>
2021-06-06 14:37:20 +0900

    C_s2

--- Assets/Script/BoardManager.cs
+++ Assets/Script/BoardManager.cs
@@ -8,17 +8,17 @@ public class BoardManager : MonoBehaviour
     void Start()
     {
         manager = GameObject.Find("GameManager");
-    }
+                    }
     public void PointerEnter()
-    {
+                    {
         GetComponent<Renderer>().material = color[2];
-    }
+                        }
     public void PointerExit()
-    {
+                        {
         GetComponent<Renderer>().material = color[0];
-    }
+                        }
     public void PointerClick()
-    {
+            {
         int x = (int)(GetComponent<Transform>().position.x+3.5f);
         int y = (int)Mathf.Abs(GetComponent<Transform>().position.z-3.5f);
         manager.GetComponent<GameManager>().TurnDisc(y,x);
